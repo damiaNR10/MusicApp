b1b41e723060815c7b4f3a90af1d9a8e8b23376f
W57008 <dymon10@o2.pl>
2018-04-19 17:38:53 +0200

    Przycisk wstecz

--- app/src/main/java/com/example/stud/musicapp/topsongs/TopSongsActivity.java
+++ app/src/main/java/com/example/stud/musicapp/topsongs/TopSongsActivity.java
@@ -12,5 +12,14 @@ public class TopSongsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_songs);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+
+        onBackPressed();
+        return true;
     }
 }
