ed601ac04199d540ec4033e2bf2eab299644e3e0
W57008 <dymon10@o2.pl>
2018-04-19 18:56:02 +0200

    Elementy

--- app/src/main/java/com/example/stud/musicapp/api/ApiClient.java
+++ app/src/main/java/com/example/stud/musicapp/api/ApiClient.java
@@ -1,8 +1,17 @@
 package com.example.stud.musicapp.api;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 /**
  * Created by W57008 on 2018-04-19.
  */
 
 public interface ApiClient {
+
+    @GET("trending.php")
+    Call<TrendingList> getTrendingList(@Query("country") String country, @Query("type") String type, @Query("format") String format);
+
+
 }
--- app/src/main/java/com/example/stud/musicapp/topsongs/TopSongsActivity.java
+++ app/src/main/java/com/example/stud/musicapp/topsongs/TopSongsActivity.java
@@ -2,11 +2,22 @@ package com.example.stud.musicapp.topsongs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.stud.musicapp.R;
+import com.example.stud.musicapp.api.ApiService;
+import com.example.stud.musicapp.api.TrendingList;
+import com.google.gson.Gson;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class TopSongsActivity extends AppCompatActivity {
 
+    RecyclerView rvList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +25,31 @@ public class TopSongsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_top_songs);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        rvList = findViewById(R.id.rvList);
+
+        Call<TrendingList> trendingListCall = ApiService.getService().getTrendingList("us", "itunes", "singles");
+
+        trendingListCall.enqueue(new Callback<TrendingList>() {
+            @Override
+            public void onResponse(Call<TrendingList> call, Response<TrendingList> response) {
+                TrendingList trendingList = response.body();
+
+                Toast.makeText(TopSongsActivity.this, "Elementow: " + trendingList.trending.size(), Toast.LENGTH_SHORT).show();
+
+                Log.d("TAG", new Gson().toJson(trendingList));
+
+
+            }
+
+            @Override
+            public void onFailure(Call<TrendingList> call, Throwable t) {
+
+                Toast.makeText(TopSongsActivity.this, "Blad pobierania danych: " + t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 
     @Override
