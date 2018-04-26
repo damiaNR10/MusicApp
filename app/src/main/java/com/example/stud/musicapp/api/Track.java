bc196ac2055f7449c3edd36809cd6dd5cee2085e
W57008 <dymon10@o2.pl>
2018-04-26 19:03:47 +0200

    Pobieranie danych szczegolowych

---
+++ app/src/main/java/com/example/stud/musicapp/api/Track.java
@@ -0,0 +1,15 @@
+package com.example.stud.musicapp.api;
+
+/**
+ * Created by W57008 on 2018-04-26.
+ */
+
+public class Track {
+
+    public String strAlbum;
+    public String strDescriptionEN;
+    public String strGenre;
+    public String strStyle;
+    public String strTrackThumb;
+
+}
--- app/src/main/java/com/example/stud/musicapp/topsongs/TopSongsAdapter.java
+++ app/src/main/java/com/example/stud/musicapp/topsongs/TopSongsAdapter.java
@@ -1,9 +1,11 @@
 package com.example.stud.musicapp.topsongs;
 
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.stud.musicapp.R;
@@ -36,12 +38,24 @@ public class TopSongsAdapter extends RecyclerView.Adapter<TopSongsAdapter.TopSon
     @Override
     public void onBindViewHolder(TopSongsViewHolders holder, int position) {
 
-        TrendingSingle trendingSingle = trending.get(position);
+        final TrendingSingle trendingSingle = trending.get(position);
 
         holder.tvPlace.setText(String.valueOf(trendingSingle.intChartPlace));
         holder.tvTrack.setText(trendingSingle.strTrack);
         holder.tvArtist.setText(trendingSingle.strArtist);
         holder.tvAlbum.setText(trendingSingle.strAlbum);
+
+        holder.llContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), SongDetailsActivity.class);
+                intent.putExtra(SongDetailsActivity.TRACK, trendingSingle.strTrack);
+                intent.putExtra(SongDetailsActivity.ARTIST, trendingSingle.strArtist);
+                intent.putExtra(SongDetailsActivity.TRACK_ID, trendingSingle.idTrack);
+
+                view.getContext().startActivity(intent);
+            }
+        });
 
     }
 
@@ -52,6 +66,7 @@ public class TopSongsAdapter extends RecyclerView.Adapter<TopSongsAdapter.TopSon
 
     public class TopSongsViewHolders extends RecyclerView.ViewHolder {
 
+        LinearLayout llContainer;
         TextView tvPlace;
         TextView tvTrack;
         TextView tvArtist;
@@ -61,6 +76,7 @@ public class TopSongsAdapter extends RecyclerView.Adapter<TopSongsAdapter.TopSon
         public TopSongsViewHolders(View itemView) {
             super(itemView);
 
+            llContainer = itemView.findViewById(R.id.llContainer);
             tvPlace = itemView.findViewById(R.id.tvPlace);
             tvTrack = itemView.findViewById(R.id.tvTrack);
             tvArtist = itemView.findViewById(R.id.tvArtist);
